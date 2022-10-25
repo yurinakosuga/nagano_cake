1c1639bfab0e62208c61966091d20b7cbffc7c56
EC2 Default User <ec2-user@ip-172-31-24-183.ap-northeast-1.compute.internal>
2022-10-25 15:59:16 +0000

    商品新規登録画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,3 +1,21 @@
 class Admin::ItemsController < ApplicationController
+  def index
+    @item = Item.new
+    @items = Item.all
+  end
   
+  def new
+  end
+  
+  def create
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
 end
