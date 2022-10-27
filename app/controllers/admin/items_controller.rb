1ad07d9130af5f8913d0f155818e374159e6d6db
EC2 Default User <ec2-user@ip-172-31-24-183.ap-northeast-1.compute.internal>
2022-10-27 13:21:59 +0000

    商品一覧画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -13,7 +13,8 @@ class Admin::ItemsController < ApplicationController
     # 3. データをデータベースに保存するためのsaveメソッド実行
     item.save
     
-    redirect_to '/admin/items'
+    
+    redirect_to admin_items_path
     
   end
   
@@ -29,7 +30,7 @@ class Admin::ItemsController < ApplicationController
   private
   # ストロングパラメータ
   def item_params
-    params.require(:item).permit(:name, :introduction, :price)
+    params.require(:item).permit(:name, :introduction, :price, :genre_id, :is_active)
   end
 
 end
