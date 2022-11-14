c538286b7ae00f401bfbb7c4d4690aa92fdd1b13
EC2 Default User <ec2-user@ip-172-31-24-183.ap-northeast-1.compute.internal>
2022-11-14 05:49:44 +0000

    更新

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -18,11 +18,21 @@ class Public::OrdersController < ApplicationController
   end  
   
   def confirm
-    
+    @order = Order.new(order_params)
+    binding.pry #追記する@order = Order.new(order_params)
+    @address = Address.find(params[:order][:address_id])
+    @order.postal_code = @address.postal_code
+    @order.address = @address.address
+    @order.name = @address.name
   end  
   
   def complete
     
-  end  
+  end
+  
+  private
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+  end
   
 end
