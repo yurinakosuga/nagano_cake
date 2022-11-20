3cccb9c439c712cedd1534fa068f5c62bd2adbaa
EC2 Default User <ec2-user@ip-172-31-24-183.ap-northeast-1.compute.internal>
2022-11-20 04:41:13 +0000

    更新

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
     (price * 1.1).floor
   end
   
-  def image
+  def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
