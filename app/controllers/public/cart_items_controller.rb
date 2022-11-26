4aaac20d506b881bcb43a358ab94025639b9e46b
EC2 Default User <ec2-user@ip-172-31-24-183.ap-northeast-1.compute.internal>
2022-11-26 10:37:02 +0000

    カート同一商品追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,10 +7,22 @@ class Public::CartItemsController < ApplicationController
   
   
   def create
-    cart_item = CartItem.new(cart_item_params)
-    cart_item.customer_id = current_customer.id
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    cart_item.save
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    @cart_items = current_customer.cart_items.all
+    count = 0
+        @cart_items.each do |cart_item|
+          if cart_item.item_id==@cart_item.item_id
+          cart_item.amount = cart_item.amount + @cart_item.amount
+          cart_item.save
+          count += 1
+          end
+         
+        end
+          if count == 0
+            @cart_item.save
+          end
+    
    
     # 4. トップ画面へリダイレクト
     redirect_to '/cart_items'
@@ -23,7 +35,9 @@ class Public::CartItemsController < ApplicationController
   end
   
   def destroy
-    
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to '/cart_items'
   end
   
   def destroy_all
--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -59,4 +59,8 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+   protected
+  def after_sign_in_path_for(resource)
+    customer_path(current_customer.id)
+  end
 end
