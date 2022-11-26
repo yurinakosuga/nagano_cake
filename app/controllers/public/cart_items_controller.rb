0e01862e6ff8237bfd9d6818393af1196855b988
EC2 Default User <ec2-user@ip-172-31-24-183.ap-northeast-1.compute.internal>
2022-11-26 16:01:48 +0000

    カート画面

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -41,7 +41,8 @@ class Public::CartItemsController < ApplicationController
   end
   
   def destroy_all
-    current_user.cart_items.destroy_all
+    current_customer.cart_items.destroy_all
+    redirect_to '/cart_items'
     
   end
   
